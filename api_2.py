99b28ac43bbcf1f7e0cab1b55aca16270bb4f83f
Diptark Bose <diptark.bose@gmail.com>
2017-02-24 19:41:08 +0530

    Add files via upload

---
+++ api_2.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+app=Flask(__name__)
+
+languages=[{'name': 'Japanese', 'traits':'easy'}, {'name': 'Chinese','traits':'difficult'}]
+lang=[]
+@app.route('/', methods=['GET'])
+def test():
+	return jsonify({'message' : 'It works!'})
+
+@app.route('/lang', methods=['GET'])
+def returnall():
+	print languages[0]
+	return jsonify ({'languages': languages})
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+	
+	for elem in languages:
+		if elem['name']==name or elem['traits']==name:
+			lang.append(elem)
+
+	return jsonify ({'languages': lang[0]})
+
+
+
+if __name__== '__main__':
+	app.run(debug=True, port=8080)
